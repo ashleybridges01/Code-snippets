7f8659a4d61beee847ed6632b1759c3fe20a0a9e
Ashley Bridges <ashleybridges01@gmail.com>
2020-04-11 09:40:16 +1000

    execution code

--- Ruby/authenticator.rb
+++ Ruby/authenticator.rb
@@ -6,3 +6,25 @@ users = [
     { username: "Garret", password: "pw4" },
     { username: "Wendy", password: "pw5" }
 ]
+
+# execution workflow
+
+puts "Welcome to MessageMe"
+25.times { print "-" }
+puts
+puts "Login page"
+
+attempts = 1
+while attempts < 4
+    print "Username: "
+    username = gets.chomp
+    print "Password: "
+    password = gets.chomp
+    auth = auth_user(username, password, users)
+    puts auth
+    puts "Press any key to continue -- Press q to quit"
+    input = gets.chomp.downcase
+    break if input == "q"
+    attempts += 1
+end
+puts "You have exceeded the number of attempts" if attempts == 4

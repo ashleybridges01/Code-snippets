1470530256776e6a87f177f1e5ab47b7c1169e2e
Ashley Bridges <ashleybridges01@gmail.com>
2020-04-11 09:31:13 +1000

    auth

---
+++ Ruby/authenticator.rb
@@ -0,0 +1,8 @@
+# users array
+users = [
+    { username: "Ashley", password: "pw1" },
+    { username: "Bruce", password: "pw2" },
+    { username: "Mike", password: "pw3" },
+    { username: "Garret", password: "pw4" },
+    { username: "Wendy", password: "pw5" }
+]

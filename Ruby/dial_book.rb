4e77510ec0ab2338aaa84cad4a1fb7b2475f7cc5
Ashley Bridges <ashleybridges01@gmail.com>
2020-04-20 14:55:39 +1000

    dial book program

---
+++ Ruby/dial_book.rb
@@ -0,0 +1,13 @@
+dial_book = {
+  "newyork" => "212",
+  "newbrunswick" => "732",
+  "edison" => "908",
+  "plainsboro" => "609",
+  "sanfrancisco" => "301",
+  "miami" => "305",
+  "paloalto" => "650",
+  "evanston" => "847",
+  "orlando" => "407",
+  "lancaster" => "717"
+}
+

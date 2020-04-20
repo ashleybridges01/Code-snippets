c4db2c3366498da79f5c586311a185409df56f78
Ashley Bridges <ashleybridges01@gmail.com>
2020-04-20 16:40:26 +1000

    dial book working

--- Ruby/dial_book.rb
+++ Ruby/dial_book.rb
@@ -19,3 +19,18 @@ def get_area_code(dial_book, key)
     dial_book[key]
 end
 
+loop do 
+    puts "Do you want to lookup an area code based on a city name (Y/N)"
+    answer = gets.chomp.downcase
+    break if answer != "y"
+    puts "Which city do you want the area code for?"
+    puts get_city_names(dial_book)
+    puts "Enter your selection"
+    prompt = gets.chomp
+    if dial_book.include?(prompt)
+        puts "The area code for #{prompt} is #{get_area_code(dial_book, prompt)}"
+    else
+        puts "You have entered an invalid city"
+    end
+end
+
